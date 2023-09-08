1caac0fd4680be9c388881d1c8d76a6a9c709de8
Bhavani Prasad <54599402+imbhavaniprasad@users.noreply.github.com>
2023-09-08 22:59:02 +0530

    Time: 1 ms (98.34%) | Memory: 39.7 MB (40.18%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+        long end = x;
+        long start = 0;
+        while(start<=end){
+         long mid = start + (end-start)/2;
+         if(mid*mid==x) return (int)mid;
+         else if(mid*mid<x){
+             start = mid+1;
+         }
+         else end= mid-1;
+        }
+return (int)end;
+    }
+}
