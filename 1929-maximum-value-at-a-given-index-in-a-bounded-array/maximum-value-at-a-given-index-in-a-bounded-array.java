a246aa9d19feeafa2550f80519c1c5043020a21f
Bhavani Prasad <54599402+imbhavaniprasad@users.noreply.github.com>
2023-09-13 01:12:54 +0530

    Time: 1 ms (94.50%) | Memory: 39.5 MB (43.48%) - LeetSync

--- 1929-maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.java
+++ 1929-maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.java
@@ -1,20 +1,22 @@
 class Solution {
     public int maxValue(int n, int index, int maxSum) {
-        int emptyR = n - index - 1;
-        int emptyL = index;
-        long h = maxSum, l = 1; // Change int to long for h and l
-        int res = 0; // Change int to long for res
+        int rightRange = n - index - 1;
+        int leftRange = index;
+        long h = maxSum, l = 1; // Change int to long for h and l coz mid could be big value
+        int res = 0; // Change int to long for res coz we return int value
         while (l <= h) {
             long mid = (h - l) / 2 + l; // Change int to long for mid
             long leftSum = 0, rightSum = 0, el = mid - 1;
-            if (emptyR <= el)
-                rightSum = (el * (el + 1)) / 2 - ((el - emptyR) * (el - emptyR + 1)) / 2;
+            //if left or right range is less than mid, apply //n*(n+1)/2-(mid-leftRange)*(mid-leftRange+1)/2;
+            //if left or right range exceeds than mid,then all those values must be 1
+            if (rightRange <= el)
+                rightSum = (el * (el + 1)) / 2 - ((el - rightRange) * (el - rightRange + 1)) / 2;
             else
-                rightSum = (el * (el + 1)) / 2 + (emptyR - el);
-            if (emptyL <= el)
-                leftSum = (el * (el + 1)) / 2 - ((el - emptyL) * (el - emptyL + 1)) / 2;
+                rightSum = (el * (el + 1)) / 2 + (rightRange - el);
+            if (leftRange <= el)
+                leftSum = (el * (el + 1)) / 2 - ((el - leftRange) * (el - leftRange + 1)) / 2;
             else
-                leftSum = (el * (el + 1)) / 2 + (emptyL - el);
+                leftSum = (el * (el + 1)) / 2 + (leftRange - el);
             long sum = leftSum + mid + rightSum;
             if (sum <= maxSum) {
                 l = mid + 1;
